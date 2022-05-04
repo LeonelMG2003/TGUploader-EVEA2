95b213ef81c01b2d9302ffc33679e326c72c1574
LeonelMG2003 <89432777+LeonelMG2003@users.noreply.github.com>
2022-05-03 20:10:21 -0400

    Add files via upload

---
+++ comfig.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = '5139068064:AAFfQmIE-dvmFoFjg4-mOY0O4PZcn9PEclY'
+API_ID = '9514816'
+API_HASH = '2f5b35e5872ddbfe1d4a7885f241044c'
+ADMIN_ID = [1088031964]
+
+MOODLE_URL = 'https://evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
+PARTES_MB = 200
+
